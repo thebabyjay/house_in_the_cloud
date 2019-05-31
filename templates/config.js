f61173e151b65c5adbcbd0559b90eed4ca706a05
Jason Barnett <jayb21@neo.rr.com>
2019-05-30 23:08:07 -0400

    renamed a setup folder from `templates` to `setup`

--- templates/config.js
+++
@@ -1,9 +0,0 @@
-const deviceTypes = require('./device/types');
-const deviceCategories = require('./device/categories');
-
-module.exports = {
-	satelliteName: `Jason's Room 4ft`,
-	deviceType: deviceTypes.LIGHT_MULTICOLOR,
-	deviceCategory: deviceCategories.LIGHT,
-	mainServerUrl: 'http://173.89.163.14:3000'
-}
